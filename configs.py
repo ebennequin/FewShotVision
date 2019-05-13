79a1691cb7bfdbdfc2716c0cfd570a09252938fc
Sicara <etienne.bennequin@gmail.com>
2019-05-13 15:37:23 +0200

    Update save_dir in config

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-save_dir                    = '/work/newriver/wyharveychen/CloserLookFewShot/'
+save_dir                    = './save_dir'
 data_dir = {}
 data_dir['CUB']             = './filelists/CUB/' 
 data_dir['miniImagenet']    = './filelists/miniImagenet/' 
