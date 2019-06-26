04a2a347c95411808892976a25897a2edb5b5855
Sicara <etienne.bennequin@gmail.com>
2019-06-26 11:53:11 +0200

    Change test for ListDataset

--- src/yolov3/utils/tests/datasets_test.py
+++ src/yolov3/utils/tests/datasets_test.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader
 class TestListDataset:
 
     def test_list_dataset_has_correct_shape(self):
-        list_path = './data/coco/5k.txt'
+        list_path = './src/loaders/tests/small_image_list.txt'
 
         dataset = ListDataset(list_path)
 
@@ -13,20 +13,3 @@ class TestListDataset:
         assert len(item[1].shape) == 3
         assert len(item[2].shape) == 2
         assert item[2].shape[1] == 6
-
-    def test_data_loader_has_expected_shape(self):
-
-        list_path = './data/coco/5k.txt'
-
-        dataset = ListDataset(list_path)
-
-        dataloader = DataLoader(
-            dataset,
-            batch_size=8,
-            shuffle=True,
-            num_workers=12,
-            pin_memory=True,
-            collate_fn=dataset.collate_fn,
-        )
-        temp = next(iter(dataloader))
-        print(temp)
