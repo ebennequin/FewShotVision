2b2515b4b29d4a4b815e0fcbc0359db0513b6f4e
Sicara <etienne.bennequin@gmail.com>
2019-07-04 12:11:55 +0200

    Add script for downloading tiny Darknet weights

--- scripts/downloaders/download_yolo_weights.sh
+++ scripts/downloaders/download_yolo_weights.sh
@@ -5,5 +5,7 @@ mkdir weights
 wget -c -P ./weights https://pjreddie.com/media/files/yolov3.weights
 # # Download weights for tiny YOLOv3
 wget -c -P ./weights https://pjreddie.com/media/files/yolov3-tiny.weights
-# Download weights for backbone network
+# Download weights for backbone network (Darknet)
 wget -c -P ./weights https://pjreddie.com/media/files/darknet53.conv.74
+# Download weights for tiny Darknet
+wget -c -P ./weights https://pjreddie.com/media/files/tiny.weights
