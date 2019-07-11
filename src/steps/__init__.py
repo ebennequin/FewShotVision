a08f880268ded421d6243e69b8b8ffc73bb47cfa
Sicara <etienne.bennequin@gmail.com>
2019-07-11 12:24:55 +0200

    Add complete YOLOMAMLCreateEpisode

--- src/steps/__init__.py
+++ src/steps/__init__.py
@@ -4,6 +4,7 @@ from .method_training import MethodTraining
 from .fetch_model import FetchModel
 from .yolomaml_training import YOLOMAMLTraining
 from .yolomaml_create_dic import YOLOMAMLCreateDic
+from .yolomaml_create_episode import YOLOMAMLCreateEpisode
 
 
 __all__ = [
@@ -13,4 +14,5 @@ __all__ = [
     'FetchModel',
     'YOLOMAMLTraining',
     'YOLOMAMLCreateDic',
+    'YOLOMAMLCreateEpisode'
 ]
--- src/steps/yolomaml_create_episode.py
+++ src/steps/yolomaml_create_episode.py
@@ -6,14 +6,16 @@ import pickle
 
 from src.utils import configs
 
-from src.loaders.dataset import create_dict_images_per_label
+from src.loaders.dataset import DetectionTaskSampler
 from src.yolov3.utils.datasets import ListDataset
 from src.yolov3.utils.parse_config import parse_data_config
 
 
 class YOLOMAMLCreateEpisode(AbstractStep):
     """
-    This step creates the dictionary requires for yolo MAML
+    This step creates a detection task. It will output :
+        - a .txt file containing the paths to the images composing the episode
+        - a .data file containing the episode configuration
     """
 
     def __init__(
@@ -22,14 +24,30 @@ class YOLOMAMLCreateEpisode(AbstractStep):
             n_way,
             n_shot,
             n_query,
+            output_dir='./data/coco/episodes',
+            episode_name=None,
             labels=None,
     ):
+        """
+
+        Args:
+            dataset_config (str): path to data config file
+            n_way (int): number of different classes in the episode
+            n_shot (int): number of support set instances per class
+            n_query (int): number of query set instances per class
+            output_dir (str): output directory
+            episode_name (str): name of the output file (without type extension)
+            labels (list): labels from which to sample instances. If None, labels will be sampled at random.
+        """
 
         self.data_config = parse_data_config(dataset_config)
         self.n_way = n_way
         self.n_shot = n_shot
         self.n_query = n_query
-        self.labels = labels if labels is not None else self.get_random_labels()
+        self.labels = labels
+
+        self.episode_name = episode_name
+        self.output_dir = output_dir
 
     def apply(self):
         """
@@ -37,24 +55,51 @@ class YOLOMAMLCreateEpisode(AbstractStep):
 
         """
 
-        with open(self.data_config['valid_dict'], 'rb') as dictionary_file:
-            images_per_label = pickle.load(dictionary_file)
+        dataset = ListDataset(self.data_config['eval'])
+
+        sampler = DetectionTaskSampler(
+            dataset,
+            self.n_way,
+            self.n_shot,
+            self.n_query,
+            1,
+            self.data_config['eval_dict_path'],
+        )
+
+        if self.labels is None:
+            self.labels = list(sampler.sample_labels())
+        else:
+            assert len(self.labels) == self.n_way, 'You have to provide exactly n_way labels'
+            assert all(
+                label in sampler.label_list for label in self.labels
+            ), "At least one label doesn't have enough instances in the dataset"
+
+        image_indices = list(sampler.sample_images_from_labels(np.array(self.labels))[self.n_way:])
 
         images_path = []
 
-        for label in self.labels:
-            images_from_label = np.random.choice(
-                self.images_per_label[label],
-                self.n_support+self.n_query,
-                replace=False
-            )
-            images_path.extend(images_from_label)
+        for index in image_indices:
+            images_path.append(dataset[index][0])
+
+        output_name = self.get_output_name()
+
+        with open(output_name + '.txt', 'w') as f:
+            for path in images_path:
+                f.write(path + '\n')
+
+        with open(output_name + '.data', 'w') as f:
+            f.write('labels='+str(self.labels).strip('[]')+'\n')
+            f.write('eval='+output_name+'.txt'+'\n')
+            f.write('classes='+str(self.data_config['classes'])+'\n')
+            f.write('names='+str(self.data_config['names'])+'\n')
 
     def dump_output(self, _, output_folder, output_name, **__):
         pass
 
-    def get_random_labels(self):
-        labels_number = self.data_config['classes']
-        labels = np.sort(np.random.choice(labels_number, self.n_way, replace=False))
+    def get_output_name(self):
+        if self.episode_name is None:
+            self.episode_name = 'task'
+            for label in self.labels:
+                self.episode_name = self.episode_name + '-' + str(label)
 
-        return labels
+        return os.path.join(self.output_dir, self.episode_name)
